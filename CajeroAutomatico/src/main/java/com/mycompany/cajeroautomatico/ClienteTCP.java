68d454b00c657b85ae5cf91a1467361cce7c6672
lnoriega <leonoriega128@gmail.com>
2024-10-24 02:08:36 -0300

    Menu cajero automatico

--- CajeroAutomatico/src/main/java/com/mycompany/cajeroautomatico/ClienteTCP.java
+++ CajeroAutomatico/src/main/java/com/mycompany/cajeroautomatico/ClienteTCP.java
@@ -19,91 +19,81 @@ import java.util.logging.Logger;
  */
 public class ClienteTCP {
 
-    
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
-                Socket sock = null;
-                Scanner sn = new Scanner(System.in);
-                sn.useDelimiter("\n");
-                
-		//PrintWriter sockOut = null;
-		//BufferedReader sockIn = null;
-		try {   
-                        sock = new Socket("localhost", 7777); // the communication socket.
-			//sockOut = new PrintWriter(sock.getOutputStream(), true); //force write
-			//sockIn = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-                        
-                        //
-                        DataInputStream in = new DataInputStream(sock.getInputStream());
-                        DataOutputStream out = new DataOutputStream(sock.getOutputStream());
-                        // Escribe el nombre y se lo manda al servidor
-                        
-                        out.writeUTF("1");
-                        
-                        // Leer mensaje del servidor
-                        String mensaje = in.readUTF();
-                        System.out.println(mensaje);
-                        
-                        String NumCuenta = sn.next();
-                        out.writeUTF(NumCuenta);
-                        
-                        // opcion Menu
-                         String Menu = in.readUTF();
-                         System.out.println(Menu);
-                         Menu = sn.next();
-                         //
-                         out.writeUTF(Menu);
-                         Menu = in.readUTF();
-                         System.out.println(Menu);
-                         // 
-                         Menu = sn.next();
-                         out.writeUTF(Menu);
-                         //
-                         Menu = in.readUTF();
-                         out.writeUTF(Menu);
-                         System.out.println(Menu); 
-                         String mensajeMonto = sn.next();
-                        out.writeUTF(mensajeMonto);
-                         
-                        String mensajeMonto2 = in.readUTF();  // Espera el mensaje del servidor
-                        out.writeUTF(mensajeMonto2);    // Imprime "Has seleccionado: Transferencia\nIngrese el monto a transferir:"
-                        System.out.println(mensajeMonto2); 
-                 
+        Socket sock = null;
+        Scanner sn = new Scanner(System.in);
+        sn.useDelimiter("\n");
 
-                        // ejecutamos el hilo
-                        ClienteHilo hilo = new ClienteHilo(in, out);
-                        hilo.start();
-                        hilo.join();
-                        
-		} catch (UnknownHostException e) {
-		System.err.println("host unreachable: localhost");
-		System.exit(1);
-		} catch (IOException e) {
-			System.err.println("cannot connect to: localhost");
-			System.exit(1);
-		} catch (InterruptedException ex) {
-            Logger.getLogger(ClienteTCP.class.getName()).log(Level.SEVERE, null, ex);
-        } 
-		//Scanner scan = new Scanner(System.in);
-		//String message = scan.next().toLowerCase();
-		//while (! message.equals("no")) {
-			//sockOut.println(message);
-			//String recu = sockIn.readLine();
-			//System.out.println("server -> client:" + recu);
-			//message = scan.next().toLowerCase();
-		//}
-		//sockOut.close();
-		//sockIn.close();  
-		//Scanner scan = new Scanner(System.in);
-		//String message = scan.next().toLowerCase();
-		//while (! message.equals("no")) {
-			//sockOut.println(message);
-			//String recu = sockIn.readLine();
-			//System.out.println("server -> client:" + recu);
-			//message = scan.next().toLowerCase();
-		//}
-		//sockOut.close();
-		//sockIn.close(); 
+        //PrintWriter sockOut = null;
+        //BufferedReader sockIn = null;
+        try {
+            sock = new Socket("localhost", 7777); // the communication socket.
+            //sockOut = new PrintWriter(sock.getOutputStream(), true); //force write
+            //sockIn = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+
+            //
+            DataInputStream in = new DataInputStream(sock.getInputStream());
+            DataOutputStream out = new DataOutputStream(sock.getOutputStream());
+            // Escribe el nombre y se lo manda al servidor
+
+            //out.writeUTF("2");
+            System.out.println("Bienvenido al sistema de Cajero Automatico. Ingrese numero de cuenta");
+            String idCuenta = sn.nextLine();
+            out.writeUTF("CajeroAutomatico," + idCuenta + ",0,0");
+            String idCuentaExiste = in.readUTF();
+
+            int opcion = 0;
+            do {
+
+                float fondosCuenta = 0;
+                // Enviar opciones al cliente
+                System.out.println("\nHola " + idCuentaExiste + " elige una opción:\n"
+                        + "1. Retiro\n"
+                        + "2. Depósito\n"
+                        + "3. Transferencia\n"
+                        + "4. Salir\n"
+                        + "Ingrese el número de la opción:");
+
+                // Leer la opción seleccionada por el cliente
+                String opcionStr = sn.nextLine();
+                switch (opcionStr) {
+                    case "1":
+                        System.out.println("ingrese monto a descontar");
+                        String montoPagar = sn.nextLine();
+                        out.writeUTF("Descuento," + idCuenta + "," + montoPagar);
+                        break;
+                    case "2":
+                        System.out.println("ingrese monto a depositar");
+                        String montoDepositar = sn.nextLine();
+                        out.writeUTF("Deposito," + idCuenta + "," + montoDepositar);
+                        break;
+                    case "3":
+                        System.out.println("ingrese ID del destino a transferir");
+                        String idDestino = sn.nextLine();
+                        System.out.println("ingrese monto a transferir");
+                        String montoTrnasferencia = sn.nextLine();
+                        out.writeUTF("Transferencia," + idCuenta + "," + montoTrnasferencia + "," + idDestino);
+                        System.out.println("Transferencia exitosa: " + in.readUTF());
+                        break;
+                    default:
+                        break;
+                }
+
+                System.out.println("Operacion exitosa: " + in.readUTF());
+                try {
+                    opcion = Integer.parseInt(opcionStr);
+                } catch (NumberFormatException e) {
+                    System.out.println("Opción no válida, por favor ingrese un número.");
+                    continue; // Volver al inicio del bucle
+                }
+            } while (opcion != 4);
+
+        } catch (UnknownHostException e) {
+            System.err.println("host unreachable: localhost");
+        } catch (IOException e) {
+            System.err.println("cannot connect to: localhost");
+        }
     }
-    
+
 }
